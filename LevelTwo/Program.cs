92afdff7825641309ef2b21ebd18d84741016a45
FranciscoAnjos <fcariaanjos@gmail.com>
2025-03-20 17:29:18 +0000

    parse args and show img

--- LevelTwo/Program.cs
+++ LevelTwo/Program.cs
@@ -6,9 +6,24 @@ namespace LevelTwo
     {
         private static void Main(string[] args)
         {
-            CanvasImage image = new CanvasImage ("tux.jpg");
-            image.MaxWidth(20);
-            AnsiConsole.Write(image) ;
+            string defaultImagePath = "tux.jpg";
+            string imagePath;
+            int width;
+
+            if (args.Length == 2)
+            {
+                imagePath = args[0];  
+                width = int.Parse(args[1]);  
+            }
+            else
+            {
+                imagePath = defaultImagePath;
+                width = 24;
+            }
+
+            CanvasImage image = new CanvasImage(imagePath);
+            image.MaxWidth(width); 
+            AnsiConsole.Write(image);
         }
     }
 }
